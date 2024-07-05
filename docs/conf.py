5402e37ebf4c21318687232343391295faa7837d
jtasse <james.tasse@gmail.com>
2024-07-05 04:47:27 -0400

    Update pages deployment

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,19 @@ release = '0.8'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'myst_parser',
+]
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
+
+html_theme = 'sphinx_rtd_theme'  # If using the ReadTheDocs theme
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
